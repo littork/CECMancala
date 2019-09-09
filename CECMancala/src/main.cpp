70bea220a29f84940995b3b5f917de02945230b5
Dylan Pozarnsky <dylan.pozarnsky@cecstudents.org>
2019-09-09 13:48:36 -0600

    Worked

--- CECMancala/src/main.cpp
+++ CECMancala/src/main.cpp
@@ -3,11 +3,7 @@
 
 	Dylan Pozarnsky 8/14/2019
 
-	Guessing game based on:
-		State machine
-		Rendering system
-		Input system
-		Sigslot mechanism
+	Rock Paper Scissors Project
 
 	The engine is very incomplete.
 */
@@ -21,45 +17,38 @@
 #include <cstdlib>
 #include <memory>
 
+std::string parsePick(const unsigned __int8 pick) {
+	switch (pick) {
+	case 0:
+		return "Rock";
+		break;
+	case 1:
+		return "Paper";
+		break;
+	case 2:
+		return "Scissors";
+		break;
+	}
+	return "";
+}
+
 int main() {
 	Engine::init();
 
 	std::string activeEntry = "";
-	unsigned __int64 maxScore = 0;
-	unsigned __int64 lastGuess = 0;
 
-	unsigned __int64 actualNumber = 0;
-
-	unsigned int totalGuesses = 0;
-	unsigned int totalGames = 0;
-	unsigned int currentGuesses = 0;
-	unsigned int mostGuesses = 0;
-	unsigned int leastGuesses = 0;
-
-	std::string guessResult = "";
+	unsigned __int8 computerPick = 0;
 
 	// Renderer
 	Slot_Scoped<> SlotRender = Engine::SignalRender.connect([&]() {
 		switch (State::getState()) {
 		case 0:
-			Renderer::drawText(0, 0, "Enter the maximum number you wish to guess for: " + activeEntry);
+			Renderer::drawText(0, 0, "Do you pick (r)ock, (p)aper, or (s)cissors? " + activeEntry);
 			break;
 		case 1:
-			Renderer::drawText(0, 0, "Guess a number between 0 and " + std::to_string(maxScore) + ": " + activeEntry);
+			Renderer::drawText(0, 0, "Your pick: " + parsePick(computerPick));
+			Renderer::drawText(0, 1, "Computer pick: " + parsePick(computerPick));
 			break;
-		case 2:
-			Renderer::drawText(0, 0, "You guessed " + std::to_string(lastGuess) + ". That number is " + guessResult);
-			break;
-		case 3:
-			Renderer::drawText(0, 0, "That number is out of range, press enter to try again");
-			break;
-		}
-
-		Renderer::drawText(0, 1, "Total Guesses: " + std::to_string(totalGuesses));
-		Renderer::drawText(0, 2, "Total Games Played: " + std::to_string(totalGames));
-		Renderer::drawText(0, 3, "Most/Least Guesses: " + std::to_string(mostGuesses) + "/" + std::to_string(leastGuesses));
-		if (totalGuesses > 0 && totalGames > 0) {
-			Renderer::drawText(0, 4, "Average number of guesses taken to end each game: " + std::to_string((float)totalGuesses / (float)totalGames));
 		}
 	});
 
@@ -72,44 +61,21 @@ int main() {
 
 		switch (oldState) {
 		case 0:
-			maxScore = std::stoi(activeEntry);
-			actualNumber = rand() % (maxScore + 1);
-			currentGuesses = 0;
+			if (activeEntry[0] != 'R' && activeEntry[0] != 'P' && activeEntry[0] != 'S') {
+				activeEntry = "";
+				State::setState(0);
+				return;
+			}
 			break;
 		}
 
 		switch (newState) {
 		case 0:
-			maxScore = 0;
-			activeEntry = "";
-			lastGuess = 0;
 			break;
 		case 1:
-			if (lastGuess == actualNumber) {
-				State::setState(0);
-			}
-			activeEntry = "";
+			computerPick = std::rand() % 3;
 			break;
 		case 2:
-			totalGuesses++;
-			currentGuesses++;
-			lastGuess = std::stoi(activeEntry);
-			activeEntry = "";
-			if (lastGuess > maxScore) {
-				State::setState(3);
-			}
-
-			guessResult = "";
-			if (lastGuess > actualNumber) {
-				guessResult = "too high.";
-			} else if (lastGuess < actualNumber) {
-				guessResult = "too low.";
-			} else {
-				guessResult = "correct! Press enter to restart";
-				break;
-			}
-			guessResult += " Press enter to guess again";
-
 			break;
 		}
 
@@ -135,52 +101,10 @@ int main() {
 				return;
 			}
 
-			if (key >= '0' && key <= '9') {
+			if (key >= 'A' && key <= 'Z') {
 				activeEntry += key;
 				Engine::render();
 				return;
-			}
-			break;
-		case 1:
-			if (key == Key::Return) {
-				if (activeEntry.length()) {
-					State::setState(2);
-				}
-			}
-
-			if (key == Key::Backspace) {
-				if (activeEntry.length()) {
-					activeEntry = activeEntry.substr(0, activeEntry.length() - 1);
-				}
-				Engine::render();
-				return;
-			}
-
-			if (key >= '0' && key <= '9') {
-				activeEntry += key;
-				Engine::render();
-				return;
-			}
-			break;
-		case 2:
-			if (lastGuess == actualNumber) {
-				if (currentGuesses > mostGuesses) {
-					mostGuesses = currentGuesses;
-				}
-				if (leastGuesses > currentGuesses || leastGuesses == 0) {
-					leastGuesses = currentGuesses;
-				}
-				totalGames++;
-			}
-
-			if (key == Key::Return) {
-				State::setState(1);
-			}
-			break;
-		case 3:
-			if (key == Key::Return) {
-				totalGames++;
-				State::setState(0);
 			}
 			break;
 		}
